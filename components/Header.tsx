d6702efc627251ed5c1e1d19d82619dfa64aa9ce
“[Naeem <miannaeem.mct@gmail.com>
2024-11-10 11:26:22 +0500

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='h-[134px] w-[1920px] bg-[#A29875] text-[75px] font-bold font-[Rye] pl-[20px] mt-[36px]'>MANZZARI</div>
+  )
+}
+
+export default Header
